ebcdf85dcddee98b63bafcbff2b4e46103181bcd
Raymundo Mateos <raymundo@MacBook-Air-de-Raymundo.local>
2026-05-22 13:03:45 -0600

    update: work time showing correctly

--- lib/widgets/work_time.dart
+++ lib/widgets/work_time.dart
@@ -7,10 +7,30 @@ class WorkTime extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        '${workDuration.inMinutes.toString().padLeft(2, '0')}:${(workDuration.inSeconds % 60).toString().padLeft(2, '0')}',
-        style: TextStyle(fontSize: 120, fontWeight: FontWeight.bold),
+    return SizedBox(
+      height: 100,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FittedBox(
+            child: Text(
+              '${workDuration.inHours.toString().padLeft(2, '0')}:',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+            ),
+          ),
+          FittedBox(
+            child: Text(
+              '${(workDuration.inMinutes % 60).toString().padLeft(2, '0')}:',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+            ),
+          ),
+          FittedBox(
+            child: Text(
+              '${(workDuration.inSeconds % 60).toString().padLeft(2, '0')} ',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+            ),
+          ),
+        ],
       ),
     );
   }
